70f628066737a42142bb0705702909b442b21dd7
Gabriel Arcenio <gabrielarcenio1999@hotmail.com>
2024-11-14 15:48:16 -0300

    Revert "use auth for homepage"
    This reverts commit e4b8e0c19e0dce8f0721e701e391220830e0e333.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,30 @@
+import Link from "next/link";
 import { db } from "../server/db";
 import { headers } from "next/headers";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-async function Images(){
-  const images = await db.query.images.findMany({
-    orderBy: (model, {desc}) => desc(model.id),
-  });
-  return (
-    <div className="flex flex-wrap gap-4">
-
-    {[...images,...images,...images].map((image) => (
-        <div key={image.id} className="w-48 flex flex-col">
-          <img src={image.url} />
-          <div className="text-sm">{image.name}</div>
-        </div>
-      ))}
-      </div>
-  )
-}
 
 
 export default async function HomePage() {
   headers();
 
-
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
 
   return (
     <main className="">
+      <div className="flex flex-wrap gap-4">
 
-    <SignedOut>
-      <div className="w-full h-full text-2xl text-center ">Acessa ai em cima</div>
-    </SignedOut>
-    <SignedIn>
-      <Images />
-    </SignedIn>
-
+      {[...images,...images,...images].map((image) => (
+          <div key={image.id} className="w-48 flex flex-col">
+            <img src={image.url} />
+            <div className="text-sm">{image.name}</div>
+          </div>
+        ))
+        }</div>
+      Hello (gallery in progress)
     </main>
   );
 }
