d9e1c118313f124f3fa26fabc1a88e8c6dc820ad
Gabriel Arcenio <gabrielarcenio1999@hotmail.com>
2024-11-17 13:47:57 -0300

    seila

--- src/app/common /full-page-image-view.tsx
+++ src/app/common /full-page-image-view.tsx
@@ -2,5 +2,15 @@ import {  getImage } from "~/server/queries";
 
 export async function FullPageImageView(props: {id: number}) {
     const image = await getImage(props.id);
-    return <img src={image.url} className='w-96'/>
+    return( 
+    <div className="flex w-full h-full min-w-0 ">
+        <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} className= 'flex-shrink  object-contain'/>
+        </div>
+   
+    <div className="w-48 flex flex-col flex-shrink-0 border-1">
+        <div className="text-x1 font-bold">{image.name}</div>
+    </div>
+    </div>
+    )
 }
